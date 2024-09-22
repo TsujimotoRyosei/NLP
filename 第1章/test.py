4dc1524898575888c46c288be80c0cc1381baf4d
ryosei15 <c1107582@planet.kanazawa-it.ac.jp>
2024-09-22 13:32:46 +0900

    9問目まで

--- 第1章/test.py
+++ 第1章/test.py
@@ -55,5 +55,76 @@ sequence = "I am an NLPer"
 word_sequence = sequence.split()
 answer = n_gram(2, word_sequence)
 print("05の答え(単語bi-gram): ",answer)
-answer = n_gram(2, sequence)
+list_sequence = list(sequence)
+list_sequence = [i for i in list_sequence if i != " "] # 空白を削除
+answer = n_gram(2, list_sequence)
 print("05の答え(文字bi-gram): ",answer)
+
+# 06. 集合
+# “paraparaparadise”と”paragraph”に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，
+# XとYの和集合，積集合，差集合を求めよ．さらに，’se’というbi-gramがXおよびYに含まれるかどうかを調べよ．
+sequence1 = "paraparaparadise"
+sequence2 = "paragraph"
+sequence1 = list(sequence1)
+sequence2 = list(sequence2)
+sequence1 = n_gram(2, sequence1)
+sequence2 = n_gram(2, sequence2)
+X = set(map(tuple,sequence1))
+Y = set(map(tuple,sequence2))
+print(X)
+answer = X | Y
+print("06の答え(和集合): ",answer)
+answer = X & Y
+print("06の答え(積集合): ",answer)
+answer = X - Y
+print("06の答え(差集合): ",answer)
+answer = ("s","e") in X
+print("06の答え(Xにseが含まれるか): ",answer)
+answer = ("s","e") in Y
+print("06の答え(Yにseが含まれるか): ",answer)
+
+# 07. テンプレートによる文生成
+# 引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．さらに，x=12, y=”気温”, z=22.4として，実行結果を確認せよ．
+def template(x, y, z):
+    return f"{x}時の{y}は{z}"
+
+x = 12
+y = "気温"
+z = 22.4
+result = template(x, y, z)
+print("07の答え: ",result)
+
+# 08. 暗号文
+# 与えられた文字列の各文字を，以下の仕様で変換する関数cipherを実装せよ．
+# 英小文字ならば(219 - 文字コード)の文字に置換
+# その他の文字はそのまま出力
+# この関数を用い，英語のメッセージを暗号化・復号化せよ．
+def cipher(character):
+    answer = ""
+    for c in character:
+        if c.islower():
+            answer += chr(219 - ord(c))
+        else:
+            answer += c
+    return answer
+
+
+# 09. Typoglycemia
+# スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，
+# それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．
+# ただし，長さが４以下の単語は並び替えないこととする．
+# 適当な英語の文（例えば”I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind .”）
+# を与え，その実行結果を確認せよ．
+import random
+
+character = "I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+character = character.split()
+answer = []
+for word in character:
+    if len(word) <= 4:
+        answer.append(word)
+    else:
+        middle = list(word[1:-1])
+        random.shuffle(middle)
+        answer.append(word[0] + ''.join(middle) + word[-1])
+print("09の答え: ",' '.join(answer))
